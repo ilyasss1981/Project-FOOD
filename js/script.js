bf3add683c3be25fd0bb29193c3a51decc6f3ea8
ILYAS NASIROV <ilyasss1981@gmail.com>
2020-11-01 18:46:46 +0300

    Lesson 61 Slider 1

--- js/script.js
+++ js/script.js
@@ -184,16 +184,16 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    const getResource = async (url) => {
-        const res = await fetch(url);
-
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
-        return await res.json();
-    };
-
     // --------------Вариант 1-----------------------
+
+    // const getResource = async (url) => {
+    //     const res = await fetch(url);
+
+    //     if (!res.ok) {
+    //         throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    //     }
+    //     return await res.json();
+    // };    
 
     // getResource('http://localhost:3000/menu')
     //     .then(data => {
@@ -310,6 +310,52 @@ window.addEventListener('DOMContentLoaded', () => {
             prevModalDialog.classList.remove('hide');
             closeModal();
         }, 4000);
-    }       
+    }     
+    
+    //----------------SLIDER---------------
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          totalSlides = document.querySelector('#total'),
+          currentSlide = document.querySelector('#current');
+    let slideIndex = 1;    
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        totalSlides.textContent = `0${slides.length}`;
+    } else {
+        totalSlides.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        if (slides.length < 10) {
+            currentSlide.textContent = `0${slideIndex}`;
+        } else {
+            currentSlide.textContent = slideIndex;
+        }        
+
+        slides.forEach(item => item.style.display = 'none');
+        slides[slideIndex - 1].style.display = 'block';
+    }
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);        
+    });
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
 });
 
